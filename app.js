08ce0730179fefa65809a7a58b09ff33e8657122
Ginnie <ginniexst@gmail.com>
2024-10-06 11:06:37 +0900

    change code

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-let inputTodo = document.getElementById("inputTodo");
-let btn = document.getElementById("addBtn");
-let input = inputTodo.value;
+let tasks = [];
 
-let todoListDatabase = [
-    {
-        id: 1,
-        name: "Do laundry",
-        status: true
-    }
-]
+let taskInput = document.getElementById("taskInput");
+let addBtn = document.getElementById("addBtn");
+let error = document.getElementById("error");
+let taskList = document.getElementById("taskList");
 
-btn.onclick = function(){
-    if (inputTodo.value === "") {
-        console.log("Vui lòng nhập vào nội dung công việc");
-    } else if (input.length() > 30) {
-        console.log("Vui lòng nhập nội dung công việc có độ dài nhỏ hơn 30 ký tự");
-    } else {
-        let todo = {
-            id: Math.random(),
-            name: inputTodo.value,
-            status: false
-        }
-        console.log(todo);
-        todoListDatabase.push(todo);
-    }
+function addTask(taskName) {
+    tasks.push(taskName);
+    renderTask();
 }
+
+function renderTask() {
+    taskList.innerHTML = ``;
+    tasks.forEach( task => {
+        let li = document.createElement("li");
+        li.textContent = task;
+        taskList.appendChild(li);
+    });
+}
+
+function showError(message) {
+    error.textContent = message;
+}
+
+function clearError(message) {
+    error.textContent = message;
+}
+
+function clearInput() {
+    taskInput.value = ``;
+}
+
+addBtn.addEventListener("click", () => {
+    let taskName = taskInput.value.trim();
+
+    if (taskName === ``) {
+        showError("Vui lòng nhập vào nội dung công việc");
+    } else if (taskName.length > 30) {
+        showError("Vui lòng nhập nội dung công việc có dộ dài nhỏ hơn 30 ký tự");
+    } else {
+        clearError()
+        addTask(taskName);
+        clearInput();
+    }
+})
